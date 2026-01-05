9038318f0795d8201d1d08ebcb58ef4e40d2e1dc
Harsh Patel <hpatel25177@gmail.com>
2026-01-05 21:40:58 +0530

    Learning node js

--- math.js
+++ math.js
@@ -1,7 +1,12 @@
-const sum = (a,b)=> a+b;
-const mul = (a,b)=> a*b;
-const g = 9.8;
-const PI = 3.14;
+// const sum = (a,b)=> a+b;
+// const mul = (a,b)=> a*b;
+// const g = 9.8;
+// const PI = 3.14;
 
-//To export some data we use 
-module.exports = sum(3,4); // we can use it in other file
+// //To export some data we use 
+// module.exports = sum(3,4); // we can use it in other file
+
+export const sum = (a,b)=> a+b;
+export const mul = (a,b)=> a*b;
+export const g = 9.8;
+export const PI = 3.14;
--- script.js
+++ script.js
@@ -10,5 +10,16 @@
 
 //Data transfer from one directory to another
 //When i need data of fruit folder
-let info = require("./Fruits");
-console.log(info);
+// let info = require("./Fruits");
+// console.log(info);
+
+// we use math.js in this file using import
+
+import {sum,PI} from "./math.js";
+import {generate,count} from "random-words";
+
+const sm = sum(1,5);
+console.log(sm);
+console.log(PI);
+
+console.log(generate());

0821bea3ece25ab6cbbe15ae164613473bf3af2b
Harsh Patel <hpatel25177@gmail.com>
2026-01-21 20:41:23 +0530

    H

--- EJSDir/index.js
+++ EJSDir/index.js
@@ -1,29 +1,30 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const instaData = require("./data.json");
 
 let port = 3000;
 
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname, "/views"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+app.get("/hello", (req, res) => {
+  res.send("Hello");
+});
+app.get("/rolldice", (req, res) => {
+  let diceVal = Math.floor(Math.random() * 6) + 1;
+  res.render("rolldice.ejs", { diceVal });
+});
+app.get("/ig/:username", (req, res) => {
+  let { username } = req.params;
+  const data = instaData[username];
+  console.log(data);
+  res.render("instagram.ejs", { data });
+});
 
-app.get("/",(req,res)=>{
-    res.render("home.ejs");
-});
-app.get("/hello",(req,res)=>{
-    res.send("Hello");
-});
-app.get("/rolldice",(req,res)=>{
-    let diceVal = Math.floor(Math.random()*6) + 1 ;
-    res.render("rolldice.ejs",{ diceVal });
-});
-app.get("/ig/:username",(req,res)=>{
-    const followers = ["adam","eve","Parker","Domanic","Johnson"];
-    let { username } = req.params;
-    res.render("instagram.ejs", { username, followers });
-})
-
-app.listen(port,()=>{
-    console.log(`Listening on port ${port}`);
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
